200198b1d0e0c39fa82c578d70ce4bb26552cc29
phantomramen <ali.hernandez9two@gmail.com>
2020-09-12 16:56:11 -0600

    first push

---
+++ employeeTrackerSchema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE auctions(
+  id INT NOT NULL AUTO_INCREMENT,
+  employee_name VARCHAR(30) NOT NULL,
+  department VARCHAR(30) NOT NULL,
+  salary INT NOT NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
